b0fdf227e9ea6a816b8b932620c67a8c1e04dc29
mikazuki-hitoha <kuro15sigu11re@outlook.jp>
2021-07-20 11:56:46 +0000

    データ追加と画像修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 [
   {
+    id: 1,
     name: '八戸市博物館',
     holiday: '月曜日',
     opening_time: '9:00',
@@ -9,6 +10,7 @@
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipNwAQ76Rv5DuhUDVVvhdqqLiRw23niyR3J-vXkR=w426-h240-k-no'
   },
   {
+    id: 2,
     name: '八甲田山雪中行軍遭難資料館',
     holiday: 'なし',
     opening_time: '9:00',
@@ -18,6 +20,7 @@
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipOFMjz5FB4B7seQTZggvSuzzX0J6zqyqLqBWRsp=w408-h306-k-no'
   },
   {
+    id: 3,
     name: '青森近代文学館',
     holiday: 'なし',
     opening_time: '9:00',
@@ -27,6 +30,7 @@
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipOV8JC4bDbTLhsjJ9Q3BZisLwN7xCE5ARJn-SfQ=w408-h306-k-no'
   },
   {
+    id: 4,
     name: '青森市森林博物館',
     holiday: '月曜日',
     opening_time: '9:00',
@@ -36,6 +40,7 @@
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipMSJmlxVfBvcyrhk1C_cGsfeGd61sUQCRxzyhAB=w408-h306-k-no'
   },
   {
+    id: 5,
     name: 'あおもり北のまほろば歴史館',
     holiday: 'なし',
     opening_time: '9:00',
@@ -45,6 +50,7 @@
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipPrK92V7ej12hgM5iSlsImO0OS34ljF5kBGOEYO=w408-h272-k-no'
   },
   {
+    id: 6,
     name: '青森県立郷土館',
     holiday: '現在臨時休館中',
     opening_time: '9:00',
@@ -54,6 +60,7 @@
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipNuutMpy3IGohvJkXFr-DYq0Ner__qrBlMHVxSK=w408-h306-k-no'
   },
   {
+    id: 7,
     name: '青森市中世の館',
     holiday: '月曜日',
     opening_time: '9:00',
@@ -61,6 +68,76 @@
     address: '〒038-1311 青森県青森市浪岡大字浪岡岡田４３',
     phone_number: '0172-62-1020',
     image_url: 'https://lh5.googleusercontent.com/p/AF1QipOTBCH57Uoim1EqUg9UmL1j3tcLeBfSr8LgJYGj=w408-h272-k-no'
+  },
+  {
+    id: 8,
+    name: '板柳町立郷土資料館',
+    holiday: '日曜日・月曜日・水曜日・金曜日',
+    opening_time: '9:00',
+    closing_time: '16:00',
+    address: '〒038-3672 青森県北津軽郡板柳町灰沼岩井７０',
+    phone_number: '0172-72-0330',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipMgAfIez4vk6Y6BVzjRjDFZsvFovyffUqaeN9p3=w426-h240-k-no'
+  },
+  {
+    id: 9,
+    name: '中泊町博物館',
+    holiday: '月曜日',
+    opening_time: '9:00',
+    closing_time: '16:45',
+    address: '〒037-0305 青森県北津軽郡中泊町中里紅葉坂２１０',
+    phone_number: '0173-69-1111',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipMtVNQrS1AJjABn3sHiU2ZEDhNDB4e4_B6pXEQ0=w408-h306-k-no'
+  },
+  {
+    id: 10,
+    name: 'つがる市縄文住居展示資料館 カルコ',
+    holiday: '月曜日',
+    opening_time: '9:00',
+    closing_time: '16:00',
+    address: '〒038-3138 青森県つがる市木造若緑５９−１',
+    phone_number: '0173-42-6490',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipPTrP7laIfayxIdHWeH2iCVvgi-nM59Mk4CJlSz=w426-h240-k-no'
+  },
+  {
+    id: 11,
+    name: '高岡の森　弘前藩歴史館',
+    holiday: 'なし',
+    opening_time: '9:30',
+    closing_time: '16:30',
+    address: '〒036-1344 青森県弘前市高岡獅子沢１２８−１１２',
+    phone_number: '0172-83-3110',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipOo4w3dh_vZ9SJ9bR7rT0hnNqo4fQbFf37e7fFx=w426-h240-k-no'
+  },
+  {
+    id: 12,
+    name: '弘前市立博物館',
+    holiday: '第3月曜日・年末年始',
+    opening_time: '9:30',
+    closing_time: '16:30',
+    address: '〒036-8356 青森県弘前市下白銀町１−６ 弘前公園内',
+    phone_number: '0172-35-0700',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipNlxNtxHqcrwkLtkOAMMm0_BBmrEQNGTtmGAVzD=w426-h240-k-no'
+  },
+  {
+    id: 13,
+    name: '弘前城史料館',
+    holiday: 'なし',
+    opening_time: '9:00',
+    closing_time: '17:00',
+    address: '〒036-8356 青森県弘前市下白銀町１ 弘前城天守内',
+    phone_number: '0172-33-8739',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipPjd2WjW5ZiFAKMguWBItOExeJcWD3f397aqGFq=w408-h306-k-no'
+  },
+  {
+    id: 14,
+    name: '田舎館村埋蔵文化財センター',
+    holiday: '月曜日',
+    opening_time: '10:00',
+    closing_time: '17:00',
+    address: '〒038-1111 青森県南津軽郡田舎館村高樋大曲６３',
+    phone_number: '0172-43-8555',
+    image_url: 'https://lh5.googleusercontent.com/p/AF1QipOb88sSwkR-nkwvxZO7gMIaO_yzZAzUmjzJ2S2Q=w408-h270-k-no'
   },
 ].each do |data|
   museum = Museum.find_or_initialize_by(id: data[:id])
